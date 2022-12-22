ed3596365b56526fa56b5930488c87aa4d08c280
JongTTing <ejh990521@gmail.com>
2022-12-23 02:50:31 +0900

    임의로 커밋

---
+++ src/main/java/com/example/firstproject/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long>{
+    Optional<MemberEntity> findByEmail(String userEmail);
+    long countBy();
+    void deleteByEmail(String email);
+}
